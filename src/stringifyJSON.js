e19ff65ef8fa5dd0083e9ac31618b92f785755b5
dblanchard13 <davidb.prof@gmail.com>
2015-01-23 18:13:19 -0800

    implemented stringifyJSON
    using a lot of ‘if’ statements and some recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+   var objType = typeof (obj);
+    if(objType==='undefined'){return undefined};
+    if(objType==='function'){return undefined};
+    if(obj === null ) {return '' + obj;}
+    if(objType==='number'){return '' + obj};
+    if(objType==='boolean'){return '' + obj};
+    if(objType==='string'){return '"' + obj + '"'};
+
+  if (Array.isArray(obj)){
+    var result = [];
+    for (var i = 0; i< obj.length ; i++){
+      if (stringifyJSON(obj[i]) === undefined)
+        result.push('null');
+      else
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+  else{
+    var finalRes = '';
+    for (var key in obj){
+      if (finalRes !== ''){
+        finalRes = finalRes + ',';
+      }
+      if (stringifyJSON(obj[key]) !== undefined){
+        finalRes = finalRes + '"' + key + '":' + stringifyJSON(obj[key]);
+      }
+    }
+    return '{' + finalRes + '}';
+  }
 };
+
+
